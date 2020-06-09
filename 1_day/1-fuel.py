d3b66962598ece9a9d952b8e99c7fad7fd4f75b2
Andre Yujiro Kondo <akondo@uolinc.com>
2020-06-09 12:48:34 -0300

    Day 1 complete

---
+++ 1_day/1-fuel.py
@@ -0,0 +1,11 @@
+def calculate_fuel(mass):
+    '''Calculate fuel divide mass by 3, round down, then subtract 2'''
+    return int(mass/3) - 2
+
+
+if __name__ == "__main__":
+    sum_fuel = 0
+    with open('./input.txt') as f:
+        for line in f:
+            sum_fuel += calculate_fuel(int(line))
+    print(sum_fuel)
---
+++ 1_day/2-fuel.py
@@ -0,0 +1,17 @@
+def calculate_fuel(mass):
+    '''Calculate fuel divide mass by 3, round down, then subtract 2'''
+    total_fuel = 0
+    while True:
+        mass = int(mass/3) - 2
+        if mass > 0:
+            total_fuel += mass
+        else:
+            break
+    return total_fuel
+
+if __name__ == "__main__":
+    sum_fuel = 0
+    with open('./input.txt') as f:
+        for line in f:
+            sum_fuel += calculate_fuel(int(line))
+    print(sum_fuel)
